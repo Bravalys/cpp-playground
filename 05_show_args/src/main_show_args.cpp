a03187e39403bd62f63c45cf70f886b3abddbca9
Sabau Valeriu <Valeriu.Sabau@ctbav.ro>
2021-07-25 16:32:10 +0300

    Step5 done

---
+++ 05_show_args/src/main_show_args.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]){
+	// Make sure there are some program arguments available.
+	if (argc <= 1){
+		std::cout << "No program arguments found." << std::endl; 
+	}
+	// For loop to print every argument on a new line
+    else{
+        for (int index = 1; index < argc; ++index) {
+		    std::cout << argv[index] << std::endl;
+	    }
+    }
+    return 0;
+}
